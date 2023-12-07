6f62927b9335bf6a6a8d8ae2c70aff77f1904397
deanbirnie <deanicdl@gmail.com>
2023-12-07 09:02:32 +0000

    Task 0 - Addtion function strongly typed

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,9 @@
-def add():
+#!/usr/bin/env python3
+"""Module contains a strongly typed addition function"""
+
+
+def add(a: float, b: float) -> float:
+  """
+  This function adds two floats using strongly typed parameters
+  """
+  return a + b
