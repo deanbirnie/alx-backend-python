42a5c9c155e931e752bbe5e90762168d4cdbae43
deanbirnie <deanicdl@gmail.com>
2023-12-07 20:28:33 +0000

    Task 12 - Removed extra annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -17,7 +17,7 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     return zoomed_in
 
 
-array: Tuple[int, ...] = (12, 72, 91)
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
