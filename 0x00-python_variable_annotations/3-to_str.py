e0a477f0d797c02a14ca11ca6ba81b9b6a711152
deanbirnie <deanicdl@gmail.com>
2023-12-07 13:01:17 +0000

    Task 3 - Function that returns string representation of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module contains a function that returns th string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function takes a float as input and returns a string
+    representation of the float.
+    """
+    return str(n)
+
