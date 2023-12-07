5554f22f69913be288537cdee49c40e765d32374
deanbirnie <deanicdl@gmail.com>
2023-12-07 12:37:16 +0000

    Task 2 - Find the floor of a float strongly typed

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that determines the floor of a float"""
+
+
+def floor(n: float) -> int:
+    """This function takes a float argument and returns its floor"""
+    return int(n)
