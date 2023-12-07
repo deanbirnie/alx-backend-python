01d86d6353a30c818cb0593684dd8bc7cb79e3c1
deanbirnie <deanicdl@gmail.com>
2023-12-07 09:51:04 +0000

    Task 1 - Strongly typed string concatentation function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function uses type annotations for its parameteers and concatenates
+    two strings.
+    """
+    return str1 + str2
